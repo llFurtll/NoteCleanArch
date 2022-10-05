d66fd9306a9adc1ae23168a3d4cb894e928bd229
Daniel Melonari <daniel.melonari@SV-NTB894.local>
2022-10-05 13:36:03 -0300

    Remoção de import desnecessario

--- lib/features/note/presentation/components/editor_note.dart
+++ lib/features/note/presentation/components/editor_note.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:html_editor_enhanced/html_editor.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
